6760cf7ebb85e4def810f7e8d78b4de75e85ae6b
seth roffe <ssroffe@gmail.com>
2019-08-13 14:15:40 -0400

    fixed bug in setup_dotfiles

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -5,13 +5,18 @@ for dir in * .[^.]*; do
     ### Make sure it's a directory ###
     if [ -d "$dir" ]; then
 
+        if [ $dir = ".git" ]; then
+            ### Don't want to copy git dir ###
+            continue
+        fi
+
         if [ $dir = ".oh-my-zsh" ]; then
             ### Custom oh-my-zsh theme ###
             configs=$(ls -A $dir)
             for f in $configs; do
                 echo "Linking ${f}..."
                 if [ -d "~/.oh-my.zsh" ]; then
-                    ln -s ${PWD}/${dir}/* ~/${dir}/themes/
+                    ln -s ${PWD}/${dir}/${f} ~/${dir}/themes/
                 fi
             done
 
@@ -20,7 +25,7 @@ for dir in * .[^.]*; do
             configs=$(ls -A $dir)
             for f in $configs; do
                 echo "Linking ${f}..."
-                ln -s ${PWD}/${file} ~
+                ln -s ${PWD}/${dir}/${f} ~/
             done
 
         else
@@ -28,7 +33,7 @@ for dir in * .[^.]*; do
             configs=$(ls -A $dir)
             for f in $configs; do
                 echo "Linking ${f}..."
-                ln -s $PWD/$dir/ ~/$dir
+                ln -s ${PWD}/${dir}/${f} ~/${dir}/
             done
         fi
 
