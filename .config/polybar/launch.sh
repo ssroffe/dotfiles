267ba6adca38cc522dbb7c8d106276726bdd22f6
seth roffe <ssroffe@gmail.com>
2019-08-13 16:17:02 -0400

    added support for multi-monitor

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,9 +3,14 @@
 # Terminate already running bad instances
 killall -q polybar
 
-# Wait until the processes have been shut down
-while grep -x polybar >/dev/null; do sleep 1; done
-
 # Launch the example bar
-polybar main_bar
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1);do
+        MONITOR=$m polybar --reload top &
+        MONITOR=$m polybar --reload bottom &
+    done
+else
+    polybar --reload top &
+    polybar --reload bottom &
+fi
 
