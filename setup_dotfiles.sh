066d53cb747b960ee0810c89b90cf90e7327f745
Seth Roffe <seth@srpopvm.localdomain>
2021-05-13 14:00:22 -0400

    Changed setup_dotfiles to copy instead of link and added common apps script

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,42 +1,50 @@
 #!/bin/bash
 
 ### Loop through all directories including hidden directories
-for dir in * .[^.]*; do
-    ### Make sure it's a directory ###
-    if [ -d "$dir" ]; then
+#for dir in * .[^.]*; do
+#    ### Make sure it's a directory ###
+#    if [ -d "$dir" ]; then
+#
+#        if [ $dir = ".git" ]; then
+#            ### Don't want to copy git dir ###
+#            continue
+#        fi
+#
+#        if [ $dir = ".oh-my-zsh" ]; then
+#            ### Custom oh-my-zsh theme ###
+#            configs=$(ls -A $dir)
+#            for f in $configs; do
+#                echo "Linking ${f}..."
+#                if [ -d "~/.oh-my.zsh" ]; then
+#                    ln -sf ${PWD}/${dir}/${f} ~/${dir}/themes/
+#                fi
+#            done
+#
+#        elif [ $dir = "home" ]; then
+#            ### Any home directory files ###
+#            configs=$(ls -A $dir)
+#            for f in $configs; do
+#                echo "Linking ${f}..."
+#                ln -sf ${PWD}/${dir}/${f} ~/
+#            done
+#
+#        else
+#            ### .config folder ###
+#            configs=$(ls -A $dir)
+#            for f in $configs; do
+#                echo "Linking ${f}..."
+#                ln -sf ${PWD}/${dir}/${f} ~/${dir}/
+#            done
+#        fi
+#
+#    fi
+#done
+cd home
+cp .aliases_roffe ${HOME}/
+cp .vimrc ${HOME}/
+cp .tmux.conf ${HOME}/
+cp .zshrc ${HOME}/
+cp .Xmodmap ${HOME}/
 
-        if [ $dir = ".git" ]; then
-            ### Don't want to copy git dir ###
-            continue
-        fi
-
-        if [ $dir = ".oh-my-zsh" ]; then
-            ### Custom oh-my-zsh theme ###
-            configs=$(ls -A $dir)
-            for f in $configs; do
-                echo "Linking ${f}..."
-                if [ -d "~/.oh-my.zsh" ]; then
-                    ln -sf ${PWD}/${dir}/${f} ~/${dir}/themes/
-                fi
-            done
-
-        elif [ $dir = "home" ]; then
-            ### Any home directory files ###
-            configs=$(ls -A $dir)
-            for f in $configs; do
-                echo "Linking ${f}..."
-                ln -sf ${PWD}/${dir}/${f} ~/
-            done
-
-        else
-            ### .config folder ###
-            configs=$(ls -A $dir)
-            for f in $configs; do
-                echo "Linking ${f}..."
-                ln -sf ${PWD}/${dir}/${f} ~/${dir}/
-            done
-        fi
-
-    fi
-done
-
+cd ..
+cp -r .config/rofi ${HOME}/.config/
