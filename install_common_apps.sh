12e1769e0f976cc7d85e33e9607f8614b0ba39b4
Seth Roffe <ssroffe@gmail.com>
2021-07-29 15:30:38 -0400

    Added pacman capabilities to install scripts and i3 config

--- install_common_apps.sh
+++ install_common_apps.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install ranger rofi nvim cmake
+which pacman > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    sudo pacman -Syyu
+    sudo pacman -S ranger rofi neovim cmake nodejs
+else
+    sudo apt update
+    sudo apt install ranger rofi nvim cmake
+fi
--- ohmyzsh_install.sh
+++ ohmyzsh_install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-sudo apt install -y curl zsh git
+which pacman > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    sudo pacman -S curl zsh git
+else
+    sudo apt install -y curl zsh git
+fi
 
 ## Oh-My-Zsh Install ##
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
