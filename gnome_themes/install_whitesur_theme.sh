4e69865a868b770763a4bd7246ed32d3623caaa3
Seth Roffe <ssroffe@gmail.com>
2022-04-26 21:02:47 -0400

    Added WhiteSur installation automation

---
+++ gnome_themes/install_whitesur_theme.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+##GITHUB REFERENCE: https://github.com/vinceliuice/WhiteSur-gtk-theme
+SCRIPT_DIR=$( dirname $0 ) ## Get script directory for relative pathing reasons
+
+## Check if theme directory is made ##
+if [ ! -d "${HOME}/.themes" ]; then
+    mkdir -p ${HOME}/.themes
+fi
+
+themedir="${HOME}/.themes/WhiteSurTheme"
+if [ ! -d ${themedir} ]; then
+    git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git ${themedir}
+fi
+
+${themedir}/install.sh -t purple
+
+#Change theme in gnome tweaks manually for now
+## TODO: Auto change gnome tweak settings?
+## TODO: Use dconf load / < file
+dconf load / < ${SCRIPT_DIR}/WhiteSur.conf
+## TODO: Check out https://github.com/vinceliuice/Orchis-theme
