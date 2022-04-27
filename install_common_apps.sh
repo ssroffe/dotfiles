0738c1a9fb046526c776d0773609fc357ffdbbee
Seth Roffe <ssroffe@gmail.com>
2022-04-26 21:03:28 -0400

    Added node and pip to install_common_apps

--- install_common_apps.sh
+++ install_common_apps.sh
@@ -6,5 +6,5 @@ if [ $? -eq 0 ]; then
     sudo pacman -S ranger rofi neovim cmake nodejs
 else
     sudo apt update
-    sudo apt install ranger rofi neovim cmake
+    sudo apt install ranger rofi neovim cmake nodejs universal-ctags python3-pip
 fi
