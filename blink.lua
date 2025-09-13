8bbc8c3aeb9be7d38198b2e0c96a3d9f0ea232b7
Seth Roffe <ssroffe@gmail.com>
2025-09-13 18:59:39 -0400

    Tab to complete for nvim

---
+++ blink.lua
@@ -0,0 +1,16 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    completion = {
+      -- Don't select by default
+      list = {
+        selection = {
+          auto_insert = false,
+        },
+      },
+    },
+    keymap = {
+      preset = "super-tab",
+    },
+  },
+}
